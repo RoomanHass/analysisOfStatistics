800a798066d4d27012fa55151f95f055cf0c933c
[RoomanHass] <[hasan0v.rom@yandex.ru]>
2023-02-08 21:16:40 +0300

    Немного изменил

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,6 @@ public class Main {
 
         long[] discount = {5, 10, 15, 8, 21, 3, 14};
 
-
         SalesManager SalesManager = new SalesManager(discount);
         System.out.println(SalesManager.max());
         System.out.println(SalesManager.stat());
